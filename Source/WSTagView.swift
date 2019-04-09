5ad497e2019afa667fd4e87b2405ba09fef3ca6c
Keith Norman <keithnorm@gmail.com>
2019-04-08 22:05:50 -0700

    ...

--- Source/WSTagView.swift
+++ Source/WSTagView.swift
@@ -22,7 +22,7 @@ open class WSTagView: UIView {
     fileprivate let textLabel = UILabel()
     private let _tag: WSTag?
     let arrow = UIButton(frame: .zero).tap {
-        let bundle = Bundle(forClass: self)
+        let bundle = Bundle(forClass: WSTagView)
         let dropdown = UIImage(named: "down-arrow", inBundle: bundle,compatibleWithTraitCollection: nil)!
         $0.setImage(dropdown, for: .normal)
     }
